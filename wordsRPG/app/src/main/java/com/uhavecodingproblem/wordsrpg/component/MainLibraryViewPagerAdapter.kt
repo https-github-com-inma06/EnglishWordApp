da03257b2d6dea03085d4249cd92c422528b3b7a
LeeDonghun <dlwldlwldus123@gmail.com>
2020-09-18 21:16:50 +0900

    뷰페이져 adapter 임시적으로 component  폴더 로 이동

---
+++ wordsRPG/app/src/main/java/com/uhavecodingproblem/wordsrpg/component/MainLibraryViewPagerAdapter.kt
@@ -0,0 +1,30 @@
+package com.uhavecodingproblem.wordsrpg.component
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.uhavecodingproblem.wordsrpg.ui.fragment.BasicPackageFragment
+import com.uhavecodingproblem.wordsrpg.ui.fragment.MyCustomPackageFragment
+import com.uhavecodingproblem.wordsrpg.ui.fragment.SubscribePackageFragment
+
+/**
+ * wordsrpg
+ * Class: MainLibraryViewPagerAdapter
+ * Created by pyg10.
+ * Created On 2020-09-18.
+ * Description:
+ */
+class MainLibraryViewPagerAdapter(fragment: FragmentActivity): FragmentStateAdapter(fragment) {
+
+    override fun getItemCount(): Int {
+        return 3
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        return when(position){
+            0 -> BasicPackageFragment()
+            1 -> MyCustomPackageFragment()
+            else-> SubscribePackageFragment()
+        }
+    }
+}
