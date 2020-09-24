4b3abd82e6343988f70c995a62b972cfd2fbabba
LeeDonghun <dlwldlwldus123@gmail.com>
2020-09-24 15:14:03 +0900

    내 패키지 fragment xml 간단 구성

--- wordsRPG/app/src/main/java/com/uhavecodingproblem/wordsrpg/ui/fragment/MyCustomPackageFragment.kt
+++ wordsRPG/app/src/main/java/com/uhavecodingproblem/wordsrpg/ui/fragment/MyCustomPackageFragment.kt
@@ -1,5 +1,6 @@
 package com.uhavecodingproblem.wordsrpg.ui.fragment
 
+import android.view.View
 import com.uhavecodingproblem.wordsrpg.R
 import com.uhavecodingproblem.wordsrpg.databinding.FragmentMyCustomPackageBinding
 import com.uhavecodingproblem.wordsrpg.ui.base.BaseFragment
@@ -16,5 +17,25 @@ class MyCustomPackageFragment: BaseFragment<FragmentMyCustomPackageBinding>(R.la
 
     override fun FragmentMyCustomPackageBinding.onCreateView() {
         Logger.v("실행")
+
+        thisFragment=this@MyCustomPackageFragment
+
+    }//onCreateView()끝
+
+
+
+    //내 패키지 검색 tv 클릭 event
+    fun searchMyCustomPackage(view: View){
+        Logger.v("내 패키지 검색 엑티비티 실행")
+
+
     }
-}
+
+    //새 커스텀 패키지 추가 엑티비티로 이동
+    fun moveToAddNewCustomPackageActivity(view: View){
+        Logger.v("fab 버튼 클릭 ->  새 커스텀 패키지 추가 엑티비티로 이동")
+
+    }
+
+
+}//class 끝
