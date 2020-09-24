5147dbf1ff07687dca6a2a5c01bd2407aed0774a
LeeDonghun <dlwldlwldus123@gmail.com>
2020-09-24 17:19:43 +0900

    새 커스텀 패키지 추가시  검색 될  태그  입력 부분 구현

--- wordsRPG/app/src/main/java/com/uhavecodingproblem/wordsrpg/ui/activity/AddNewCustomPackageActivity.kt
+++ wordsRPG/app/src/main/java/com/uhavecodingproblem/wordsrpg/ui/activity/AddNewCustomPackageActivity.kt
@@ -2,8 +2,12 @@ package com.uhavecodingproblem.wordsrpg.ui.activity
 
 import android.app.AlertDialog
 import android.view.View
+import android.view.animation.AlphaAnimation
+import android.view.animation.Animation
 import android.widget.Toast
 import androidx.core.view.size
+import com.google.android.material.chip.Chip
+import com.google.android.material.chip.ChipDrawable
 import com.uhavecodingproblem.wordsrpg.R
 import com.uhavecodingproblem.wordsrpg.databinding.ActivityAddNewCustomPackageBinding
 import com.uhavecodingproblem.wordsrpg.ui.base.BaseActivity
@@ -28,12 +32,87 @@ class AddNewCustomPackageActivity : BaseActivity<ActivityAddNewCustomPackageBind
     }
 
 
+    //검색용 태그 추가하기
+    fun addTag(view: View){
+
+        //tag 내용
+        val tagString= binding.editTvAddNewTag.text.toString()
+
+        when{
+
+            //태그가 3개 이상이면  태그 추가 불가
+            binding.chipGroup.size > 2 -> {
+                Toast.makeText(this,R.string.str_tag_size_max, Toast.LENGTH_SHORT).show()
+            }
+
+
+            //tag 내용중 공백이 있을때
+            tagString.contains(" ") -> {
+                Toast.makeText(this,R.string.str_tag_detect_space,Toast.LENGTH_SHORT).show()
+            }
+
+            //적은 태그내용이 없을떄
+            tagString.isEmpty() -> {
+                Toast.makeText(this,R.string.str_tag_no_content,Toast.LENGTH_SHORT).show()
+            }
+
+            //chip group 에  chip 형태 태그들 추가
+            else -> {
+                addTagViewInChipGroup(tagString)
+
+                //추가후 editext 비워주기
+                binding.editTvAddNewTag.text.clear()
+            }
+
+        }//when 끝
+
+    }//addTag() 끝
+
+
+
+    //chipgroup 에 유저가 입력한  태그 내용을
+    //chip뷰에 담아 동적 추가 진행
+    private fun addTagViewInChipGroup(tagString:String){
+
+        val chip= Chip(this)
+
+        //chip close icon 때문에 커스텀 style 추가
+        chip.setChipDrawable(ChipDrawable.createFromAttributes(this,null,0,R.style.tag_view_style))
+        chip.text = tagString//유저가 입력한 태그 내용 넣어줌.
+
+        //chipgroup에 동적 추가
+        binding.chipGroup.addView(chip)
+
+        //chip뷰에  close icon 클릭 event
+        chip.setOnCloseIconClickListener {
+
+            //투명하게  사라지는  애니메이션 적용
+            val anim = AlphaAnimation(1f, 0f)
+            anim.duration = 250
+
+            anim.setAnimationListener(object : Animation.AnimationListener {
+                override fun onAnimationRepeat(animation: Animation?) {}
+
+                override fun onAnimationEnd(animation: Animation?) {//애니메이션 끝났을때  chipgroup에서  해당 chip 삭제
+                    binding.chipGroup.removeView(it)
+                }
+
+                override fun onAnimationStart(animation: Animation?) {}
+            })
+
+            it.startAnimation(anim)
+
+        }
+    }//addTagViewInChipGroup() 끝
+
+
 
     //뒤로 가기 아이콘 클릭
     fun backIconClicked(view: View){
         Logger.v("뒤로가기 클릭")
         finish()
-    }
+    }//backIconClicked() 끝
+
 
 
     //패키지 썸네일 이미지 갤러리에서 가져오기
@@ -47,7 +126,7 @@ class AddNewCustomPackageActivity : BaseActivity<ActivityAddNewCustomPackageBind
             }
             .show()
 
-    }
+    }//pickThumbNailImageFromGallery() 끝
 
 
     //완료 버튼 event
@@ -56,7 +135,7 @@ class AddNewCustomPackageActivity : BaseActivity<ActivityAddNewCustomPackageBind
 
         // TODO: 2020-09-24 일단  엑티비티 종료로 넣어둠  나중에  패키지 추가 작업 필요
         finish()
-    }
+    }//completeAddNewCustomPackage() 끝
 
 
 
