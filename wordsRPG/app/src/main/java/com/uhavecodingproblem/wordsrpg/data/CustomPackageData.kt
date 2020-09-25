7b9816417fefad1c776bd5b562d533f70537f354
LeeDonghun <dlwldlwldus123@gmail.com>
2020-09-25 12:04:37 +0900

    커스텀 패키지 테스트를 위해   임시적으로  mockdata 및 dataclass  구성

---
+++ wordsRPG/app/src/main/java/com/uhavecodingproblem/wordsrpg/data/CustomPackageData.kt
@@ -0,0 +1,20 @@
+package com.uhavecodingproblem.wordsrpg.data
+
+/**
+ * wordsrpg
+ * Class: CustomPackageData.
+ * Created by leedonghun.
+ * Created On 2020-09-25.
+ * Description:
+ * 커스텀 패키지 데이터 클래스
+ */
+
+data class CustomPackageData(
+
+   val uid:Int,
+   val ownerUid:Int,
+   val packageName:String,
+   val profileImageUrl:String?
+
+
+)
---
+++ wordsRPG/app/src/main/java/com/uhavecodingproblem/wordsrpg/data/mockdata/CustomPackageListMockData.kt
@@ -0,0 +1,29 @@
+package com.uhavecodingproblem.wordsrpg.data.mockdata
+
+import com.uhavecodingproblem.wordsrpg.data.CustomPackageData
+
+/**
+ * wordsrpg
+ * Class: CustomPackageListMockData.
+ * Created by leedonghun.
+ * Created On 2020-09-25.
+ * Description:
+ *
+ * 임시적으로 테스트를 용 커스텀 패키지 리스트
+ */
+fun mockCustomPackageList()=
+    mutableListOf(
+        CustomPackageData(uid = 1,ownerUid = 3 ,packageName = "프로그래밍 단어장", profileImageUrl = "https://ifh.cc/g/Jn9ghK.jpg"),
+        CustomPackageData(uid = 2,ownerUid = 3 , packageName = "워킹데드1화 단어장", profileImageUrl = "https://ifh.cc/g/b6yryB.jpg"),
+        CustomPackageData(uid = 3,ownerUid = 3 , packageName = "토익준비용 단어장", profileImageUrl = "https://ifh.cc/g/PeB4iS.jpg"),
+        CustomPackageData(uid = 4,ownerUid = 3 , packageName = "오픽 단어장", profileImageUrl = "https://ifh.cc/g/5QbCWC.png"),
+        CustomPackageData(uid = 5,ownerUid = 3 , packageName = "마케팅 단어장", profileImageUrl = "https://ifh.cc/g/1kQm6l.jpg"),
+        CustomPackageData(uid = 6,ownerUid = 4 , packageName = "여행준비 단어장", profileImageUrl = "https://ifh.cc/g/Jn9ghK.jpg"),
+        CustomPackageData(uid = 7,ownerUid = 3 , packageName = "자동차 용어 단어장", profileImageUrl = "https://ifh.cc/g/0q4a2h.jpg"),
+        CustomPackageData(uid = 8,ownerUid = 4 , packageName = "이솝우화 단어장", profileImageUrl = null),
+        CustomPackageData(uid = 9,ownerUid = 3 , packageName = "영어유치원 단어장", profileImageUrl = null),
+        CustomPackageData(uid = 10,ownerUid = 3 , packageName = "컴퓨터용품 단어장", profileImageUrl = "https://ifh.cc/g/KfsEuu.jpg"),
+        CustomPackageData(uid = 11,ownerUid = 4 , packageName = "우리 신체 단어장", profileImageUrl = "https://ifh.cc/g/Wa5o7R.jpg"),
+        CustomPackageData(uid = 12,ownerUid = 4 , packageName = "프로그래밍2 단어장", profileImageUrl = "https://ifh.cc/g/Jn9ghK.jpg"),
+        CustomPackageData(uid = 13,ownerUid = 4 , packageName = "프로그래밍3 단어장", profileImageUrl = "https://ifh.cc/g/Jn9ghK.jpg")
+    )
