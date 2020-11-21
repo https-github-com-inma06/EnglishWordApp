2ce712eef8727f24e6672453a28019bc8816fbe2
Loner123 <dlwlstjq410@gmail.com>
2020-11-22 05:21:07 +0900

    [Add] 다이얼로그 설정 완료

--- wordsRPG/app/src/main/java/com/uhavecodingproblem/wordsrpg/component/battle/BattleRankingListAdapter.kt
+++ wordsRPG/app/src/main/java/com/uhavecodingproblem/wordsrpg/component/battle/BattleRankingListAdapter.kt
@@ -1,12 +1,17 @@
 package com.uhavecodingproblem.wordsrpg.component.battle
 
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.uhavecodingproblem.wordsrpg.R
+import com.uhavecodingproblem.wordsrpg.databinding.DialogBattleChallengeBinding
+import com.uhavecodingproblem.wordsrpg.databinding.DialogBattleResultBinding
 import com.uhavecodingproblem.wordsrpg.databinding.ItemBattleRankingBinding
+import com.uhavecodingproblem.wordsrpg.ui.activity.battle.BattleGameActivity
+import com.uhavecodingproblem.wordsrpg.ui.dialog.BattleDialog
 
 class BattleRankingListAdapter(
     private val imageList: MutableList<String> = mutableListOf(),
@@ -14,7 +19,28 @@ class BattleRankingListAdapter(
     private val battleScoreList: MutableList<Int> = mutableListOf(),
     private val myRanking: Int
 ) : RecyclerView.Adapter<BattleRankingListAdapter.ViewHolder>() {
-    inner class ViewHolder(val binding: ItemBattleRankingBinding) : RecyclerView.ViewHolder(binding.root)
+
+
+    inner class ViewHolder(val binding: ItemBattleRankingBinding) : RecyclerView.ViewHolder(binding.root) {
+        init {
+            itemView.setOnClickListener {
+                val dialog = BattleDialog<DialogBattleChallengeBinding>(binding.root.context, R.layout.dialog_battle_challenge)
+
+                dialog.binding.apply {
+                    this.image = imageList[adapterPosition]
+                    this.userName = nameList[adapterPosition]
+                    this.currentCase = "님에게 배틀 신청"
+                    this.btnGameStart.setOnClickListener {
+                        Intent(binding.root.context,BattleGameActivity::class.java).also {
+                            root.context.startActivity(it)
+                        }
+                        dialog.dismiss()
+                    }
+                }
+                dialog.show()
+            }
+        }
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         DataBindingUtil.inflate<ItemBattleRankingBinding>(
@@ -38,13 +64,12 @@ class BattleRankingListAdapter(
                 it.image = imageList[position]
                 it.score = battleScoreList[position]
                 it.userName = nameList[position]
-                if(position+1 >= myRanking)
+                if (position + 1 >= myRanking)
                     it.ranking = position + 2
                 else
-                it.ranking = position + 1
+                    it.ranking = position + 1
             }
         }
-
     }
 
     override fun getItemCount() = imageList.size + 1
--- wordsRPG/app/src/main/java/com/uhavecodingproblem/wordsrpg/component/battle/MainBattleNotificationListAdapter.kt
+++ wordsRPG/app/src/main/java/com/uhavecodingproblem/wordsrpg/component/battle/MainBattleNotificationListAdapter.kt
@@ -1,11 +1,17 @@
 package com.uhavecodingproblem.wordsrpg.component.battle
 
+import android.content.Intent
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.uhavecodingproblem.wordsrpg.R
+import com.uhavecodingproblem.wordsrpg.databinding.DialogBattleChallengeBinding
+import com.uhavecodingproblem.wordsrpg.databinding.DialogBattleResultBinding
 import com.uhavecodingproblem.wordsrpg.databinding.ItemMainBattleNotificationBinding
+import com.uhavecodingproblem.wordsrpg.ui.activity.battle.BattleGameActivity
+import com.uhavecodingproblem.wordsrpg.ui.dialog.BattleDialog
 
 
 class MainBattleNotificationListAdapter(
@@ -17,21 +23,67 @@ class MainBattleNotificationListAdapter(
 ) : RecyclerView.Adapter<MainBattleNotificationListAdapter.ViewHolder>() {
 
     inner class ViewHolder(val binding: ItemMainBattleNotificationBinding?) : RecyclerView.ViewHolder(binding!!.root) {
+        init {
+            binding?.apply {
+                ivUserCurrentImage.setOnClickListener {
+                    if (ivBattleAlarmMode.isSelected) {
+                        val dialogChallenge = BattleDialog<DialogBattleChallengeBinding>(
+                            root.context,
+                            R.layout.dialog_battle_challenge
+                        )
 
+                        dialogChallenge.binding.apply {
+                            this.image = imageList[adapterPosition]
+                            this.currentCase = "배틀 신청이 걸려왔습니다."
+                            this.userName = userNameList[adapterPosition]
+                            this.btnGameStart.setOnClickListener {
+                                Intent(binding.root.context, BattleGameActivity::class.java).also {
+                                    root.context.startActivity(it)
+                                }
+                                dialogChallenge.dismiss()
+                            }
+                        }
+                        dialogChallenge.show()
+                    } else {
+                        val dialogResult = BattleDialog<DialogBattleResultBinding>(
+                            root.context,
+                            R.layout.dialog_battle_result).apply {
+                            this.binding.image = imageList[adapterPosition]
+                            this.binding.userName = userNameList[adapterPosition]
+                            this.binding.result = "값 아직 없음"
+                            this.binding.currentScore = 5555555
+                            this.binding.plusScore = 100
+                            this.binding.newScore = 5555555 + 100
+                            this.binding.btnGameStart.setOnClickListener {
+                                Intent(binding.root.context,BattleGameActivity::class.java).also {
+                                    root.context.startActivity(it)
+                                }
+                            }
+                            this.binding.tvClose.setOnClickListener {
+                                onBackPressed()
+                                dismiss()
+                            }
+                        }
+                        dialogResult.show()
+                    }
+                }
+            }
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
 
-         DataBindingUtil.inflate<ItemMainBattleNotificationBinding>(
+        DataBindingUtil.inflate<ItemMainBattleNotificationBinding>(
             LayoutInflater.from(parent.context),
             R.layout.item_main_battle_notification, parent, false
         ).also {
-             return ViewHolder(it)
-         }
+            return ViewHolder(it)
+        }
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.binding?.also {
+
             it.image = imageList[position]
             it.date = battleDateList[position]
             it.userName = userNameList[position]
