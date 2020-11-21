670942881e56eda8871ce3f1367abc498347cd4f
Loner123 <dlwlstjq410@gmail.com>
2020-11-22 05:21:07 +0900

    [Add] 다이얼로그 설정 완료

--- wordsRPG/app/src/main/java/com/uhavecodingproblem/wordsrpg/component/battle/MainBattleSimilarScoreUsersAdapter.kt
+++ wordsRPG/app/src/main/java/com/uhavecodingproblem/wordsrpg/component/battle/MainBattleSimilarScoreUsersAdapter.kt
@@ -1,12 +1,16 @@
 package com.uhavecodingproblem.wordsrpg.component.battle
 
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.uhavecodingproblem.wordsrpg.R
+import com.uhavecodingproblem.wordsrpg.databinding.DialogBattleChallengeBinding
 import com.uhavecodingproblem.wordsrpg.databinding.ItemMainBattleSimilarScoreUsersBinding
+import com.uhavecodingproblem.wordsrpg.ui.activity.battle.BattleGameActivity
+import com.uhavecodingproblem.wordsrpg.ui.dialog.BattleDialog
 
 class MainBattleSimilarScoreUsersAdapter(
     private val imageList: List<String> = listOf(),
@@ -17,11 +21,31 @@ class MainBattleSimilarScoreUsersAdapter(
 
     inner class ViewHolder(v: View) : RecyclerView.ViewHolder(v) {
         val binding = DataBindingUtil.bind<ItemMainBattleSimilarScoreUsersBinding>(v)
-        init {
-                binding?.ivUserImage?.setOnClickListener {
-                    if (adapterPosition == 0)
-                    setClickEvent()
 
+        init {
+            binding?.ivUserImage?.setOnClickListener {
+                if (adapterPosition == 0)
+                    setClickEvent()
+                else {
+                    binding.ivUserImage.setOnClickListener {
+                        val dialog = BattleDialog<DialogBattleChallengeBinding>(
+                            binding.root.context,
+                            R.layout.dialog_battle_challenge
+                        ).apply {
+                            this.binding.image = imageList[adapterPosition]
+                            this.binding.userName = nameList[adapterPosition]
+                            this.binding.currentCase = "님에게 배틀 신청"
+                            this.binding.btnGameStart.setOnClickListener {
+                                Intent(binding.root.context, BattleGameActivity::class.java).also {
+                                    binding.root.context.startActivity(it)
+                                }
+                                dismiss()
+                            }
+                        }
+                        dialog.show()
+
+                    }
+                }
             }
         }
 
@@ -38,13 +62,13 @@ class MainBattleSimilarScoreUsersAdapter(
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
 
         holder.binding?.apply {
-            if(position == 0) {
-                image = imageList[position]
+            if (position == 0) {
+                image = root.context.resources.getString(R.string.imagePath).format("ic_baseline_add_32")
                 name = "더보기"
                 return@apply
             }
-                image = imageList[position]
-                name = nameList[position]
+            image = imageList[position]
+            name = nameList[position]
         }
     }
 }
