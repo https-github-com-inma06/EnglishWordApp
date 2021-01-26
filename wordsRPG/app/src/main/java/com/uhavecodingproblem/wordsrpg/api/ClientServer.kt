556442f958cd6ad9f6abe764f2264ab8a02163f6
Loner123 <dlwlstjq410@gmail.com>
2021-01-26 21:46:04 +0900

    [Modify] 마지 대기

--- wordsRPG/app/src/main/java/com/uhavecodingproblem/wordsrpg/api/ClientServer.kt
+++
@@ -1,5 +0,0 @@
-package com.uhavecodingproblem.wordsrpg.api
-//52.78.122.62
-class ClientServer {
-    // 레트로핏
-}
--- wordsRPG/app/src/main/java/com/uhavecodingproblem/wordsrpg/api/ServerApi.kt
+++
@@ -1,8 +0,0 @@
-package com.uhavecodingproblem.wordsrpg.api
-
-interface ServerApi {
-    //REST API 사용하는곳
-
-
-
-}
