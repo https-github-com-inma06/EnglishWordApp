73b1eb7a7a80d46167e13457ea9940e221f31f73
pyg100794 <pyg100794@gmail.com>
2021-01-26 20:21:14 +0900

    패키지 변경으로인한 데이터바인딩 관련 수정

--- wordsRPG/app/src/main/java/com/uhavecodingproblem/wordsrpg/ui/fragment/library/SearchCustomPackageFragment.kt
+++ wordsRPG/app/src/main/java/com/uhavecodingproblem/wordsrpg/ui/fragment/library/SearchCustomPackageFragment.kt
@@ -1,5 +1,6 @@
 package com.uhavecodingproblem.wordsrpg.ui.fragment.library
 
+import android.content.Context
 import android.view.View
 import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
